4ab7e545f3d58d1e5333ef4ea3e6dc7d49151130
Breno Rosa Barbosa <breno.barbosa422@gmail.com>
2026-05-25 23:56:13 -0300

    Inicio do servico carsimulator

---
+++ car-simulator/src/main/java/com/brenoox/carsimulator/CarsimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.brenoox.carsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarsimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarsimulatorApplication.class, args);
+	}
+
+}
---
+++ car-simulator/src/test/java/com/brenoox/carsimulator/CarsimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brenoox.carsimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarsimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
