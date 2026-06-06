c013a59fbd466e8b687ed1a37038402016e2005f
Breno Rosa Barbosa <breno.barbosa422@gmail.com>
2026-06-06 02:09:05 -0300

    Docker + Topic Config

---
+++ car-simulator/src/main/java/com/brenoox/carsimulator/infra/config/KafkaTopicConfig.java
@@ -0,0 +1,24 @@
+package com.brenoox.carsimulator.infra.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+@EnableKafka
+public class KafkaTopicConfig {
+
+    @Value("${app.kafka.telemetry-topic:car-telemetry}")
+    private String topicName;
+
+    @Bean
+    public NewTopic newTopic() {
+        return TopicBuilder.name(topicName)
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+}
